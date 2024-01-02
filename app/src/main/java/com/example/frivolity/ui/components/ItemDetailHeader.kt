1936ffade6716fef4fdf10a1f95bfdd61b04ec15
a-rosen <annierrosen@gmail.com>
2024-01-02 15:55:49 -0500

    Detail header UI cleanup

--- app/src/main/java/com/example/frivolity/ui/components/ItemDetailHeader.kt
+++ app/src/main/java/com/example/frivolity/ui/components/ItemDetailHeader.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.frivolity.R
@@ -37,57 +39,63 @@ fun ItemDetailCard(
         Text(
             text = itemDetail.name,
             style = MaterialTheme.typography.headlineSmall,
+            textAlign = TextAlign.Center,
             modifier = Modifier
-                .padding(4.dp)
+                .padding(top = 4.dp)
                 .align(Alignment.CenterHorizontally)
         )
+
+        if (itemDetail.jobToEquip?.get("Name") != null) {
+            Text(
+                text = itemDetail.jobToEquip["Name"].toString(),
+                style = MaterialTheme.typography.labelMedium,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 8.dp)
+            )
+        }
         Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
+            horizontalArrangement = Arrangement.SpaceEvenly,
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .fillMaxWidth()
-                .padding(bottom = 8.dp)
         ) {
-            Text(
-                text =
-                if (itemDetail.jobToEquip?.get("Name") != null) {
-                    itemDetail.jobToEquip["Name"].toString()
-                } else {
-                    ""
-                }
-            )
-            Text(text = "iLvl: ${itemDetail.iLevel}")
-            Text(text = "Type: ${itemDetail.type}")
-            Text(text = "equippable: ${itemDetail.levelToEquip.toString()}")
-
+            Text(text = "iLevel: ${itemDetail.iLevel}")
+            if (itemDetail.type != null) {
+                Text(text = "Type: ${itemDetail.type}")
+            }
+            if (itemDetail.levelToEquip != null) {
+                Text(text = "Level To Equip: ${itemDetail.levelToEquip}")
+            }
         }
         Text(
-            text = "\"${itemDetail.description}\"",
+            text = itemDetail.description,
             style = MaterialTheme.typography.bodySmall,
+            fontStyle = FontStyle.Italic,
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
         )
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun ItemDetailCardPreviewEquippable() {
     ItemDetailCard(
         itemDetail = UiItemDetail(
-            "Allagan Tomestone of Frivolity",
+            "Allagan Tomestone of Frivolity But Much Longer In Title",
             0,
             "",
             0,
             1,
-            mapOf(Pair("Name", "ACN BLM RDM SMN")),
+            mapOf(Pair("Name", "ABC DEF GHI JKL MNO PQR STU")),
             description = "This sure is an item that does things.",
             type = "Arms"
         ),
     )
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun ItemDetailCardPreviewRegular() {
     ItemDetailCard(
