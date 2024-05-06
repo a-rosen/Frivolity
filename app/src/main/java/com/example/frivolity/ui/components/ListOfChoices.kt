6c0374bada5eb17f748fd010cf91009fffadaa60
a-rosen <annierrosen@gmail.com>
2024-05-06 11:41:39 -0400

    clean up world selection UI

--- app/src/main/java/com/example/frivolity/ui/components/ListOfChoices.kt
+++ app/src/main/java/com/example/frivolity/ui/components/ListOfChoices.kt
@@ -1,16 +1,22 @@
 package com.example.frivolity.ui.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -18,13 +24,31 @@ fun ListOfChoices(
     choices: List<String>,
     onListItemClick: (String) -> Unit,
 ) {
-    LazyColumn() {
+    var selectedChoice by remember { mutableStateOf( "") }
+    fun onSelectionChange(stringToSelect: String) {
+        selectedChoice = stringToSelect
+    }
+    LazyColumn(
+        modifier = Modifier
+            .selectableGroup()
+    ) {
+
         items(choices) {
             ListItem(
                 headlineText = { Text(it) },
                 modifier = Modifier
-                    .clickable { onListItemClick(it) }
-                    .padding(8.dp)
+                    .clickable {
+                        onSelectionChange(it)
+                        onListItemClick(it)
+                    },
+                trailingContent = {
+                    if (it == selectedChoice) {
+                        Icon(
+                            Icons.Filled.Done,
+                            ""
+                        )
+                    }
+                },
             )
         }
     }
